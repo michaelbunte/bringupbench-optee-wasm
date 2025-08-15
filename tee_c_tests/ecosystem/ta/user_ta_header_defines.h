dd40d7a75a0764350c6aeae81257f57818b22f0b
Nicholas <nickmakaha@gmail.com>
2025-08-14 20:25:16 -0700

    connect4

--- tee_c_tests/ecosystem/ta/user_ta_header_defines.h
+++ tee_c_tests/ecosystem/ta/user_ta_header_defines.h
@@ -43,10 +43,11 @@
  */
 #define TA_FLAGS			TA_FLAG_EXEC_DDR
 
-/* Provisioned stack size */
-#define TA_STACK_SIZE			(2 * 1024)
+// DEFAULT
+// #define TA_STACK_SIZE			(2 * 1024)
+// #define TA_DATA_SIZE			(32 * 1024)
 
-/* Provisioned heap size for TEE_Malloc() and friends */
+#define TA_STACK_SIZE			(32 * 1024)
 #define TA_DATA_SIZE			(32 * 1024)
 
 /* The gpd.ta.version property */
