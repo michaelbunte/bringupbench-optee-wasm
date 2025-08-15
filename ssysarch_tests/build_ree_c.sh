c2294eda25cf836b9f457593419077c921fa7233
Nicholas <nickmakaha@gmail.com>
2025-08-15 11:33:23 -0700

    additional setup

---
+++ ssysarch_tests/build_ree_c.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf ./ree_c_ecosystem/common 2>/dev/null
+mkdir ./ree_c_ecosystem/common 
+cp ./libmin_common/* ./ree_c_ecosystem/common
+
+rm ./ree_c_ecosystem/user_ta_header_defines.h 2>/dev/null
+cp ./shared/user_ta_header_defines.h ./ree_c_ecosystem
--- ssysarch_tests/tee_c_ecosystem/cleanup.sh
+++ ssysarch_tests/tee_c_ecosystem/cleanup.sh
@@ -24,3 +24,6 @@ echo "Done."
 
 rm ./tee_c_ecosystem/ta/user_ta_header_defines.h
 rm -rf ./tee_c_ecosystem/ta/include/common
+
+rm ./ree_c_ecosystem/user_ta_header_defines.h 2>/dev/null
+rm -rf ./ree_c_ecosystem/common 2>/dev/null
