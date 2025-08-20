8fca0c7dcb74ef2a5e2296d2ea1da8fa0774275b
Nicholas <nickmakaha@gmail.com>
2025-08-20 10:41:02 -0700

    better tee c building

--- ssysarch_tests/build_tee_c_tests.sh
+++ ssysarch_tests/build_tee_c_tests.sh
@@ -4,27 +4,16 @@ rm ./built_tests/*_ta.ta
 rm ./built_tests/*.tar
 mkdir built_tests || true
 
-./build_ta.sh bubblesort
-cp ./tee_c_ecosystem/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta \
-    ./built_tests/bubblesort_ta.ta
 
-./build_ta.sh connect4
-cp ./tee_c_ecosystem/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta \
-    ./built_tests/connect4_ta.ta
+TESTS=(bubblesort connect4 lu_decomp nbody tinynn)
 
-./build_ta.sh lu_decomp
-cp ./tee_c_ecosystem/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta \
-    ./built_tests/lu_decomp_ta.ta
-
-./build_ta.sh nbody
-cp ./tee_c_ecosystem/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta \
-    ./built_tests/nbody_ta.ta
-
-./build_ta.sh tinynn
-cp ./tee_c_ecosystem/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta \
-    ./built_tests/tinynn_ta.ta
+for TEST in "${TESTS[@]}"; do
+    ./build_ta.sh $TEST
+    cp ./tee_c_ecosystem/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta \
+        ./built_tests/${TEST}_ta.ta
+done
 
 cd built_tests
 tar -cf ta_tests.tar tinynn_ta.ta nbody_ta.ta lu_decomp_ta.ta connect4_ta.ta bubblesort_ta.ta
 
-rm *.ta
+rm *.ta`
